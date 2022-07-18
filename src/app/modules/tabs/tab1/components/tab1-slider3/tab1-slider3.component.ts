2029dfc55427fac27671cc3a231562bd5b6449d8
Fabio Mercorillo <fabio.mercorillo@soprasteria.com>
2022-07-18 17:00:40 +0200

    last home slider

--- src/app/modules/tabs/tab1/components/tab1-slider3/tab1-slider3.component.ts
+++ src/app/modules/tabs/tab1/components/tab1-slider3/tab1-slider3.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ITab1SliderItem } from '../../models/i-tab1-slider-item';
 
 @Component({
   selector: 'app-tab1-slider3',
   templateUrl: './tab1-slider3.component.html',
   styleUrls: ['./tab1-slider3.component.scss'],
 })
-export class Tab1Slider3Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class Tab1Slider3Component {
+  slider: Array<ITab1SliderItem> = [{
+      img: '../../../../assets/images/slider/1.png',
+      description: 'Wireless Controller per PS5',
+      price: '64.99 €',
+      like: true
+    },
+    {
+      img: '../../../../assets/images/slider/2.png',
+      description: 'Nike Sport White - Pantaloncini uomo',
+      price: '50.5 €',
+      like: true
+    },
+    {
+      img: '../../../../assets/images/slider/3.png',
+      description: 'Guanti XC Omega - Per palestra',
+      price: '36.55 €',
+      like: true
+    },
+    {
+      img: '../../../../assets/images/slider/4.png',
+      description: 'Cuffie Logitech - Per gaming',
+      price: '20.2 €',
+      like: true
+    }
+  ];
 }
+
