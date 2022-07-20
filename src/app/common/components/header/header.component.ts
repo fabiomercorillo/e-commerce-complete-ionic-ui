e9bfa1749c54172fe6935d1878770de019022ea0
alfred_arcifa <alfred.arcifa@soprasteria.com>
2022-07-20 10:36:01 +0200

    - profile - pdp in progress

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -2,19 +2,18 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-
 export class HeaderComponent implements OnInit {
   @Input() onlyToolbar: boolean; // hide back button and title for parent components
   @Input() onlySafeArea: boolean;
   @Input() color: string;
   @Input() rate: number;
-  constructor(private nav: NavController) { }
+  @Input() borderOnBack: boolean;
+  constructor(private nav: NavController) {}
 
   ngOnInit(): void {}
 
